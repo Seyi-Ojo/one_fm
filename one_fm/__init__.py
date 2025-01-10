146e8395d73be6f3c057a6abb8bc7c8bebad4c9d
Seyi-Ojo <seyidiamond11@gmail.com>
2025-01-10 01:45:35 +0100

    ch(pytest): test 1

--- one_fm/__init__.py
+++ one_fm/__init__.py
@@ -1,56 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from one_fm.permissions import get_custom_user_permissions
+from frappe.core.doctype.user_permission import user_permission
+from one_fm.overrides.goal import get_childrens
+from one_fm.overrides.assignment_rule import do_assignment
+from frappe.automation.doctype.assignment_rule.assignment_rule import AssignmentRule
+from one_fm.overrides.stock_controller import make_batches_with_supplier_batch_id
+from erpnext.controllers.stock_controller import StockController
+from hrms.hr.doctype.goal.goal import get_children
+from hrms.hr.doctype.leave_policy_assignment.leave_policy_assignment import LeavePolicyAssignment
+from one_fm.overrides.shift_assignment import ShiftAssignmentOverride
+from hrms.hr.doctype.shift_assignment.shift_assignment import ShiftAssignment
+from hrms.hr.doctype.interview_feedback.interview_feedback import InterviewFeedback
+from hrms.hr.doctype.interview.interview import Interview
+from hrms.hr.doctype.leave_allocation.leave_allocation import LeaveAllocation
+from erpnext.stock import stock_ledger
+from one_fm.overrides.interview import validate_interview_overlap
+from one_fm.overrides.stock_ledger import get_valuation_rate_
+from one_fm.overrides.payment_entry import add_party_gl_entries_, get_valid_reference_doctypes_
+from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
+from one_fm.overrides.email_queue import prepare_email_content as email_content, get_unsubscribe_str_
+from frappe.email.doctype.email_queue.email_queue import QueueBuilder, SendMailContext
+from one_fm.overrides.employee import EmployeeOverride
+from hrms.overrides.employee_master import EmployeeMaster, validate_onboarding_process
+from one_fm.utils import post_login, validate_reports_to, custom_validate_nestedset_loop, get_existing_leave_count, custom_validate_interviewer
+from one_fm.one_fm.payroll_utils import add_tax_components
+from one_fm.api.notification import after_insert
+from frappe.desk.doctype.notification_log.notification_log import NotificationLog
+from frappe.workflow.doctype.workflow_action import workflow_action
+from one_fm.overrides.workflow import filter_allowed_users, get_next_possible_transitions, is_workflow_action_already_created_
+from one_fm.operations.doctype.contracts.contracts import calculate_item_values
+from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
+from one_fm.api.mobile.Leave_application import notify_leave_approver
+from hrms.hr.doctype.leave_application.leave_application import LeaveApplication
+from one_fm.api.doc_methods.item_price import validate, check_duplicates
+from erpnext.setup.doctype.employee.employee import Employee
+from erpnext.stock.doctype.item_price.item_price import ItemPrice
+from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+from hrms.payroll.doctype.payroll_entry.payroll_entry import PayrollEntry
 import frappe
 
-from hrms.payroll.doctype.payroll_entry.payroll_entry import PayrollEntry
-from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
-from erpnext.stock.doctype.item_price.item_price import ItemPrice
-from erpnext.setup.doctype.employee.employee import Employee
-# from one_fm.api.doc_methods.payroll_entry import fill_employee_details
-# from one_fm.api.doc_methods.salary_slip import (
-# 	get_working_days_details, get_unmarked_days_based_on_doj_or_relieving, get_unmarked_days, get_data_for_eval
-# )
-from one_fm.api.doc_methods.item_price import validate, check_duplicates
-from hrms.hr.doctype.leave_application.leave_application import LeaveApplication
-from one_fm.api.mobile.Leave_application import notify_leave_approver
-from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
-from one_fm.operations.doctype.contracts.contracts import calculate_item_values
-
-from one_fm.overrides.workflow import filter_allowed_users, get_next_possible_transitions, is_workflow_action_already_created_
-from frappe.workflow.doctype.workflow_action import workflow_action
-
-
-from frappe.desk.doctype.notification_log.notification_log import NotificationLog
-from one_fm.api.notification import after_insert
-from one_fm.one_fm.payroll_utils import add_tax_components
-from one_fm.utils import post_login, validate_reports_to, custom_validate_nestedset_loop, get_existing_leave_count, custom_validate_interviewer
-from hrms.overrides.employee_master import EmployeeMaster, validate_onboarding_process
-from one_fm.overrides.employee import EmployeeOverride
-from frappe.email.doctype.email_queue.email_queue import QueueBuilder, SendMailContext
-from one_fm.overrides.email_queue import prepare_email_content as email_content, get_unsubscribe_str_
-from frappe.workflow.doctype.workflow_action import workflow_action
-# from one_fm.utils import override_frappe_send_workflow_action_email
-from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
-from one_fm.overrides.payment_entry import add_party_gl_entries_, get_valid_reference_doctypes_
-from one_fm.overrides.stock_ledger import get_valuation_rate_
-from one_fm.overrides.interview import validate_interview_overlap
-from erpnext.stock import stock_ledger
-from hrms.hr.doctype.leave_allocation.leave_allocation import LeaveAllocation
-from hrms.hr.doctype.interview.interview import Interview
-from hrms.hr.doctype.interview_feedback.interview_feedback import InterviewFeedback
-from hrms.hr.doctype.shift_assignment.shift_assignment import ShiftAssignment
-from one_fm.overrides.shift_assignment import ShiftAssignmentOverride
-from hrms.hr.doctype.leave_policy_assignment.leave_policy_assignment import LeavePolicyAssignment
-from hrms.hr.doctype.goal.goal import get_children
-from erpnext.controllers.stock_controller import StockController
-from one_fm.overrides.stock_controller import make_batches_with_supplier_batch_id
-from frappe.automation.doctype.assignment_rule.assignment_rule import AssignmentRule
-from one_fm.overrides.assignment_rule import do_assignment
-from one_fm.overrides.goal import get_childrens
-from frappe.core.doctype.user_permission import user_permission
-from one_fm.permissions import get_custom_user_permissions
-
-
+# Define these variables first, before any imports that might use them
 __version__ = '0.0.1'
 app_name = "one_fm"
 app_title = "One Facilities Management"
@@ -60,9 +50,18 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "omar.ja93@gmail.com"
 app_license = "MIT"
-
 required_apps = ["frappe", "erpnext"]
 
+# Now do the rest of your imports
+# from one_fm.api.doc_methods.payroll_entry import fill_employee_details
+# from one_fm.api.doc_methods.salary_slip import (
+# 	get_working_days_details, get_unmarked_days_based_on_doj_or_relieving, get_unmarked_days, get_data_for_eval
+# )
+
+
+# from one_fm.utils import override_frappe_send_workflow_action_email
+
+# Then your function definitions and other code
 user_permission.get_user_permissions = get_custom_user_permissions
 StockController.make_batches = make_batches_with_supplier_batch_id
 Interview.validate_overlap = validate_interview_overlap
